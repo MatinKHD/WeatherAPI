cbd9ac98942c8872020c8b3113cad9cf7b8b46ed
Matin KHD <matink714@gmail.com>
2022-09-04 13:38:43 +0430

    create navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {WeatherComponent} from './weather/weather.component';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {WeatherModule} from "./weather/weather.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    WeatherModule,
     MatProgressSpinnerModule
   ],
   providers: [],
--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -4,15 +4,22 @@ import {CommonModule} from '@angular/common';
 import {WeatherRoutingModule} from './weather-routing.module';
 import {MatSliderModule} from "@angular/material/slider";
 import {MatButtonModule} from "@angular/material/button";
+import {WeatherComponent} from "./weather.component";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {NavbarComponent} from './navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WeatherComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     WeatherRoutingModule,
     MatSliderModule,
     MatButtonModule,
+    MatProgressSpinnerModule
   ]
 })
 export class WeatherModule {
