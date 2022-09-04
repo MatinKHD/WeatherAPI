cb0b5d38fe7290f3dc431540144528eabf392433
Matin KHD <matink714@gmail.com>
2022-09-04 12:53:16 +0430

    create routing for weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'weather',
+    loadChildren: () => import('../app/weather/weather.module').then((m) => m.WeatherModule)
+  },
+  {
+    path: '**', redirectTo: 'weather'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
