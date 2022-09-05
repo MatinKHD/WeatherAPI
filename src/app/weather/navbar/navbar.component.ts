04f8755f439e4298a240a83318fe8420023088d8
Matin KHD <matink714@gmail.com>
2022-09-05 15:06:33 +0430

    implementing autocomplete method on search-box in navbar.component.ts

--- src/app/weather/navbar/navbar.component.ts
+++ src/app/weather/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {map, Observable, startWith} from "rxjs";
+import {ApiService} from "../services/api.service";
 
 @Component({
   selector: 'app-navbar',
@@ -9,17 +10,44 @@ import {map, Observable, startWith} from "rxjs";
 })
 export class NavbarComponent implements OnInit {
   myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
+  options: any[] = [];
   filteredOptions?: Observable<string[]>;
 
-  constructor() {
+  constructor(
+    private apiService: ApiService
+  ) {
+
   }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+
+
+    console.log(this.filteredOptions);
+
+  }
+
+  changeCity(event: any) {
+    if (event.target.value) {
+      this.apiService.autocompelete(event.target.value).subscribe((value: any) => {
+        console.log('value', value);
+        for (let i = 0; i < value.length; i++) {
+          this.options.push(value[i]?.name)
+        }
+        console.log(this.options);
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filter(value || '')),
+        );
+      }, error => {
+
+      }).add(() => {
+
+      });
+    }
+  }
+
+  dastan(name: any) {
+    console.log(name);
   }
 
   private _filter(value: string): string[] {
@@ -27,5 +55,8 @@ export class NavbarComponent implements OnInit {
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
+
+
+  //
 
 }
