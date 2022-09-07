3045842243df46541250081d111da736c897625b
Matin KHD <matink714@gmail.com>
2022-09-07 15:10:29 +0430

    add some description to navbar.component.ts files method

--- src/app/weather/navbar/navbar.component.ts
+++ src/app/weather/navbar/navbar.component.ts
@@ -21,6 +21,23 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //in each word that user write in search box we check that
+  // if we have word and the length of the word
+  // is greater than 3 words or char and then store the value of
+  // search box to valueInput field
+  getValueOfSearchBox(event: any) {
+    if (event.target.value && event.target.value.length > 3) {
+      this.valueInput = event.target.value;
+    }
+  }
+
+  // we set options field to an empty array
+  // then we get the input element in html to use focus fun
+  // after that we connect to our api and put valueInput field
+  // in autoComplete method that comes from apiService
+  // after we subscribe to it we set options fields to value
+  // and after that we go to our html file and loop throw this
+  // in autoComplete tag
   changeCity() {
     this.options = [];
     document.getElementById('input')!.focus();
@@ -29,13 +46,11 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-  dastan(name: any) {
+  // get the value from search box and send to parent component
+  // in this case weather.component.ts
+  getValueFromSearchBox(name: any) {
     this.cityNameForNavbar.emit(name);
   }
 
-  getValue(event: any) {
-    if (event.target.value && event.target.value.length > 3) {
-      this.valueInput = event.target.value;
-    }
-  }
+
 }
