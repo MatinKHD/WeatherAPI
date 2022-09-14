fb0977a5b5ec87a591e294053f811cb59f7383bc
Matin KHD <matink714@gmail.com>
2022-09-14 17:20:56 +0430

    designing main-main component

--- src/app/weather/main-main/main-main.component.ts
+++ src/app/weather/main-main/main-main.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main-main',
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./main-main.component.scss']
 })
 export class MainMainComponent implements OnInit {
-
+  @Input() cityNameForMainMain: any;
   constructor() {
   }
 
